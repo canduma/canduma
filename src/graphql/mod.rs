0768c047c6decf20ea0c0ff180e53c1caf90f009
Olexiy Buyanskyy <tbot.olexiyb@gmail.com>
2020-03-30 14:14:18 +0300

    * Switch to graphql playground that has many features  see https://github.com/prisma-labs/graphql-playground * fixed "No schema found issue", playground (and graphiql) should be home page to work correctly. * properly use opt.domain parameter to avoid CORS issues

--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -5,5 +5,5 @@ pub mod model;
 
 pub(super) fn route(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/graphql").route(web::post().to(handler::graphql)))
-        .service(web::resource("/graphiql").route(web::get().to(handler::graphiql)));
+        .service(web::resource("/").route(web::get().to(handler::playground)));
 }
