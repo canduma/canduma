87d5e5ccdcf73452a53d10debb874c5a914a46e1
Jonathan Woollett-Light <jonathanwoollettlight@gmai.com>
2020-12-21 07:27:54 +0000

    Some main documentation

--- src/main.rs
+++ src/main.rs
@@ -19,47 +19,67 @@ use actix_web::middleware::Logger;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Gets enviroment variables from `.env.example`
     dotenv::dotenv().ok();
+
+    // Initiates error logger
     env_logger::init();
 
+    // Sets options to enviroment variables
     let opt = {
         use structopt::StructOpt;
         cli_args::Opt::from_args()
     };
 
+    // Database
+    let pool = database::pool::establish_connection(opt.clone());
     let schema = std::sync::Arc::new(crate::graphql::model::create_schema());
+
+    // Authorisation
     let domain = opt.domain.clone();
     let cookie_secret_key = opt.auth_secret_key.clone();
     let secure_cookie = opt.secure_cookie;
     let auth_duration = time::Duration::hours(i64::from(opt.auth_duration_in_hour));
-    let port = opt.port;
-    let pool = database::pool::establish_connection(opt.clone());
 
+    // Server port
+    let port = opt.port;
+
+    // Server
     let server = HttpServer::new(move || {
         // prevents double Arc
         let schema: web::Data<graphql::model::Schema> = schema.clone().into();
-        
+
         App::new()
+            // Database
             .data(pool.clone())
             .app_data(schema)
+            // Options
             .data(opt.clone())
+            // Error logging
             .wrap(Logger::default())
+            // Authorisation
             .wrap(IdentityService::new(
                 CookieIdentityPolicy::new(cookie_secret_key.as_bytes())
                     .name("auth")
                     .path("/")
                     .domain(&domain)
+                    // Time from creation that cookie remains valid
                     .max_age_time(auth_duration)
+                    // Restricted to https?
                     .secure(secure_cookie),
             ))
+            // Sets routes via secondary files
             .configure(user::route)
             .configure(graphql::route)
     })
+    // Running at `format!("{}:{}",port,"0.0.0.0")`
     .bind(("0.0.0.0", port))
     .unwrap()
+    // Starts server
     .run();
 
     eprintln!("Listening on 0.0.0.0:{}", port);
 
+    // Awaiting server to exit
     server.await
 }
